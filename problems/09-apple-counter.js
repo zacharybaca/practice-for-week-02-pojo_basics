c51f3a6c2639075c66faf17f70fb18b653b6e6a7
zacharybaca <zachary.baca@msn.com>
2023-06-15 20:00:19 -0500

    problem9

--- problems/09-apple-counter.js
+++ problems/09-apple-counter.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function `appleCounter(appleObj)` that takes in an object containing a 
+Write a function `appleCounter(appleObj)` that takes in an object containing a
 number of keys that have the word 'apple' contained within them. The `appleCounter`
-function will be in charge of returning the number of keys that contain the word 
+function will be in charge of returning the number of keys that contain the word
 "apple".
 
 **Hint**: you may want to take all the keys and lower case them for easier
@@ -30,6 +30,14 @@ appleCounter(obj3); // => 3
 
 function appleCounter(appleObj) {
   // Your code here
+  let counter = 0;
+
+  for (key in appleObj) {
+    if (key.includes('apples')) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
